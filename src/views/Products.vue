54a284211751c287e0d50c03c521d6203b950a7f
Michael-Nyarko-Affanyi <cwajodepay@gmail.com>
2024-10-05 18:34:07 +0000

    feat: worked single product

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -6,10 +6,17 @@ import mac from '@/assets/mac.png'
 import tv from '@/assets/tv.png'
 import fridge from '@/assets/fridge.png'
 import Carousel from 'primevue/carousel';
+import Breadcrumb from 'primevue/breadcrumb';
+import apple from '@/assets/appleBrand.png'
+import beats from '@/assets/beats.png'
+import nintendo from '@/assets/Nintendo.png'
+import samsung from '@/assets/Samsung.png'
+import playstation from '@/assets/playstation.png'
+import xbox from '@/assets/xbox.png'
 
 export default {
   name: 'ProductsPage',
-  components: { ForwardIcon, Carousel },
+  components: { ForwardIcon, Carousel, Breadcrumb },
   data() {
     return {
       electronics,
@@ -84,6 +91,42 @@ export default {
           numVisible: 1,
           numScroll: 1
         }
+      ],
+      breadcrumbItems: [
+        { label: 'Electronics' },
+        { label: 'Computer' },
+        { label: 'Accessories' },
+        { label: 'Keyboard' },
+        { label: 'Wireless' }
+      ],
+      home: {
+        icon: 'pi pi-home'
+      },
+      popularBrands: [
+        {
+          name: 'Apple',
+          image: apple
+        },
+        {
+          name: 'Beats',
+          image: beats
+        },
+        {
+          name: 'Nintendo',
+          image: nintendo
+        },
+        {
+          name: 'Playstation',
+          image: playstation
+        },
+        {
+          name: 'Samsung',
+          image: samsung
+        },
+        {
+          name: 'Xbox',
+          image: xbox
+        }
       ]
     }
   },
@@ -96,72 +139,73 @@ export default {
 </script>
 
 <template>
-  <router-view v-if="productsStore.singleCategory"></router-view>
-  <div class="px-6 py-4" v-else>
-<!--    top card-->
-    <div class="bg-primary-gray rounded-lg shadow-md py-6 px-8 flex items-center justify-between">
-      <h3 class="card-header">Delve into the world of tech and electronics</h3>
-      <div class="w-full h-[318px] ">
-        <img class="w-full h-full object-contain" :src="electronics" alt="">
-      </div>
-    </div>
+  <Breadcrumb class="text-gray-500 ml-6" :model="breadcrumbItems" />
+  <router-view></router-view>
+<!--  <div class="px-6 py-4" v-else>-->
+<!--&lt;!&ndash;    top card&ndash;&gt;-->
+<!--    <div class="bg-primary-gray rounded-lg shadow-md py-6 px-8 flex items-center justify-between">-->
+<!--      <h3 class="card-header">Delve into the world of tech and electronics</h3>-->
+<!--      <div class="w-full h-[318px] ">-->
+<!--        <img class="w-full h-full object-contain" :src="electronics" alt="">-->
+<!--      </div>-->
+<!--    </div>-->
 
-<!--    subcategories-->
-    <div class="my-6">
-      <h3 class="text-3xl text-dark-primary font-medium mb-6">Top Categories</h3>
-      <div class="grid grid-cols-2 gap-10">
-        <div
-          v-for="(topCategory, index) in topCategories"
-          :key="index"
-          :class="[
-          'bg-dark-primary cursor-pointer w-full hover:bg-blue-primary transition-colors duration-700 ease-in-out rounded-md',
-          index === 0 ? 'row-span-2' : 'row-span-1'
-          ]"
-        >
-          <div class="flex items-center justify-between p-6">
-            <h4 class="text-white text-3xl font-medium max-w-[300px]">{{topCategory.name}}</h4>
-            <ForwardIcon fill="white" width="40"/>
-          </div>
-          <div :class="[index !== 0 ? 'w-2/4 ml-auto' : '']">
-            <img :class="['w-full h-full object-cover', index !== 0 ? 'max-h-[330px]' : '']" :src="topCategory.image" alt="">
-          </div>
-        </div>
-      </div>
-    </div>
-
-<!--    other categories-->
-    <Carousel
-      :value="subCategories"
-      :numVisible="3" :numScroll="3"
-      :responsive-options="responsiveOptions"
-      class="flex justify-center"
-    >
-      <template #item="{data}">
-        <div class="w-[362px] text-center">
-          <div class="bg-primary-gray w-full h-[362px] flex items-end">
-            <img class="w-full h-4/6 object-contain" :src="data.image" :alt="data.name">
-          </div>
-          <h4 class="text-lg my-4">{{data.name}}</h4>
-        </div>
-<!--        <div class="border border-surface-200 dark:border-surface-700 rounded m-2  p-4">-->
-<!--          <div class="mb-4">-->
-<!--            <div class="relative mx-auto">-->
-<!--              <img :src="'https://primefaces.org/cdn/primevue/images/product/' + slotProps.data.image" :alt="slotProps.data.name" class="w-full rounded" />-->
-<!--              <Tag :value="slotProps.data.inventoryStatus" :severity="getSeverity(slotProps.data.inventoryStatus)" class="absolute" style="left:5px; top: 5px"/>-->
-<!--            </div>-->
+<!--&lt;!&ndash;    subcategories&ndash;&gt;-->
+<!--    <div class="my-6">-->
+<!--      <h3 class="text-3xl text-dark-primary font-medium mb-6">Top Categories</h3>-->
+<!--      <div class="grid grid-cols-2 gap-10">-->
+<!--        <div-->
+<!--          v-for="(topCategory, index) in topCategories"-->
+<!--          :key="index"-->
+<!--          :class="[-->
+<!--          'bg-dark-primary cursor-pointer w-full hover:bg-blue-primary transition-colors duration-700 ease-in-out rounded-md',-->
+<!--          index === 0 ? 'row-span-2' : 'row-span-1'-->
+<!--          ]"-->
+<!--        >-->
+<!--          <div class="flex items-center justify-between p-6">-->
+<!--            <h4 class="text-white text-3xl font-medium max-w-[300px]">{{topCategory.name}}</h4>-->
+<!--            <ForwardIcon fill="white" width="40"/>-->
 <!--          </div>-->
-<!--          <div class="mb-4 font-medium">{{ slotProps.data.name }}</div>-->
-<!--          <div class="flex justify-between items-center">-->
-<!--            <div class="mt-0 font-semibold text-xl">${{ slotProps.data.price }}</div>-->
-<!--            <span>-->
-<!--                    <Button icon="pi pi-heart" severity="secondary" outlined />-->
-<!--                    <Button icon="pi pi-shopping-cart" class="ml-2"/>-->
-<!--                </span>-->
+<!--          <div :class="[index !== 0 ? 'w-2/4 ml-auto' : '']">-->
+<!--            <img :class="['w-full h-full object-cover', index !== 0 ? 'max-h-[330px]' : '']" :src="topCategory.image" alt="">-->
 <!--          </div>-->
 <!--        </div>-->
-      </template>
-    </Carousel>
-  </div>
+<!--      </div>-->
+<!--    </div>-->
+
+<!--&lt;!&ndash;    other categories&ndash;&gt;-->
+<!--    <div class="my-6">-->
+<!--      <h3 class="text-3xl text-dark-primary font-medium mb-6">Other Categories</h3>-->
+<!--      <Carousel-->
+<!--      :value="subCategories"-->
+<!--      :numVisible="3" :numScroll="3"-->
+<!--      :responsive-options="responsiveOptions"-->
+<!--      class="flex justify-center"-->
+<!--    >-->
+<!--      <template #item="{data}">-->
+<!--        <div class="w-[362px] text-center">-->
+<!--          <div class="bg-primary-gray w-full h-[362px] flex items-end cursor-pointer hover:shadow-md transition-all duration-700 ease-in-out">-->
+<!--            <img class="w-full h-4/6 object-contain" :src="data.image" :alt="data.name">-->
+<!--          </div>-->
+<!--          <h4 class="text-lg my-4">{{data.name}}</h4>-->
+<!--        </div>-->
+<!--      </template>-->
+<!--    </Carousel>-->
+<!--    </div>-->
+
+<!--&lt;!&ndash;    Popular brands&ndash;&gt;-->
+<!--    <div class="my-6">-->
+<!--      <h3 class="text-3xl text-dark-primary font-medium mb-6">Popular Brands</h3>-->
+<!--      <div class="grid md:grid-cols-2 xl:grid-cols-3 justify-between gap-y-6">-->
+<!--        <div class="w-[382px] text-center" v-for="(brand, index) in popularBrands" :key="index">-->
+<!--          <div class="bg-primary-gray w-full h-[382px] shadow-md cursor-pointer hover:shadow-lg transition-all duration-700 ease-in-out rounded-md">-->
+<!--            <img class="w-full h-full object-contain" :src="brand.image" :alt="brand.name">-->
+<!--          </div>-->
+<!--          <h4 class="text-lg my-4">{{brand.name}}</h4>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </div>-->
 </template>
 
 <style scoped>
--- src/views/SingleProduct.vue
+++ src/views/SingleProduct.vue
@@ -1,15 +1,97 @@
 <script>
+import Tree from 'primevue/tree';
+
 export default {
-  name: 'SingleProduct'
+  name: 'SingleProduct',
+  components: { Tree },
+  data() {
+    return {
+      nodes: [
+        {
+          key: "price",
+          label: 'Price',
+          children: [
+            {
+              key: 2,
+              label: 'Electronics',
+            },
+            {
+              key: 6,
+              label: 'Clothing',
+            },
+          ],
+        },
+        {
+          key: "ratings",
+          label: 'Ratings',
+          children: [
+            {
+              key: 2,
+              label: 'Electronics',
+            },
+            {
+              key: 6,
+              label: 'Clothing',
+            },
+          ],
+        },
+        {
+          key: "capacity",
+          label: 'Capacity',
+          children: [
+            {
+              key: 2,
+              label: 'Electronics',
+            },
+            {
+              key: 6,
+              label: 'Clothing',
+            },
+          ],
+        },
+        {
+          key: "brand",
+          label: 'Brand',
+          children: [
+            {
+              key: 2,
+              label: 'Electronics',
+            },
+            {
+              key: 6,
+              label: 'Clothing',
+            },
+          ],
+        },
+      ],
+    };
+  },
+  methods: {
+    onSelectionChange(e) {
+      console.log(e);
+    },
+  },
 }
 </script>
 
 <template>
-  <div>
-    SingleProduct
+  <div class="w-full flex items-center gap-10 mx-6 my-6">
+    <aside class="w-3/12 border border-gray-100 rounded-md px-4 py-6">
+      <h4 class="text-blue-primary font-medium text-lg border-b pb-1">Filters</h4>
+      <Tree
+          :value="nodes"
+          selection-mode="checkbox"
+          @selection-change="onSelectionChange"
+      />
+    </aside>
+    <section class="basis-9/12">Products</section>
   </div>
 </template>
 
 <style scoped>
-
+:deep(.p-tree-node-content) {
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-between;
+}
 </style>
